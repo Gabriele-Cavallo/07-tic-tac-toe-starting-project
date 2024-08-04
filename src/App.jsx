431fc0b7a7f32cbf9f5a5ed06c7e3d5b77ee4316
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-08-04 19:11:54 +0200

    feat: lift state up for active player from App component to GameBoard and Player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import Player from "./components/Player"
+import GameBoard from "./components/GameBoard"
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState('X');
+
+  function handleSelectSquare(){
+    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' :'X')
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player name='player 1' symbol='X' />
-          <Player name='player 4' symbol='O' />
+        <ol id="players" className="highlight-player">
+          <Player initialName='player 1' symbol='X' isActive={activePlayer === 'X'} />
+          <Player initialName='player 2' symbol='O' isActive={activePlayer === 'O'} />
         </ol>
+          <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}></GameBoard>
       </div>
     </main>
   )
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-export default function Player({ initialName , symbol }){
+export default function Player({ initialName , symbol, isActive }){
     const [playerName, setPlayerName]= useState(initialName);
     const [ isEditing, setIsEditing] = useState(false);
     let btnText = !isEditing ? 'Edit' : 'Save'
@@ -13,7 +13,7 @@ export default function Player({ initialName , symbol }){
 
     return(
         <>
-            <li>
+            <li className={isActive ? 'active' : undefined}> 
                 <span className="player">
                     {!isEditing ? <span className="player-name">{playerName}</span> : <input type="text" required value={playerName} onChange={handleChange} />}
                     <span className="player-symbol">{symbol}</span>  
