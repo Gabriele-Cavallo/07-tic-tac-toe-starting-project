d47fa9377d082f6a5931f6f0fe5f6a0ebd836bcd
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-08-04 18:32:50 +0200

    feat: add player name input field

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,15 +1,21 @@
 import { useState } from "react";
-export default function Player({ name , symbol }){
+export default function Player({ initialName , symbol }){
+    const [playerName, setPlayerName]= useState(initialName);
     const [ isEditing, setIsEditing] = useState(false);
     let btnText = !isEditing ? 'Edit' : 'Save'
     function handleEditClick(){
-        setIsEditing(!isEditing);
+        setIsEditing((editing) => !editing);
     }
+
+    function handleChange(event){
+        setPlayerName(event.target.value);
+    }
+
     return(
         <>
             <li>
                 <span className="player">
-                    {!isEditing ? <span className="player-name">{name}</span> : <input type="text" required />}
+                    {!isEditing ? <span className="player-name">{playerName}</span> : <input type="text" required value={playerName} onChange={handleChange} />}
                     <span className="player-symbol">{symbol}</span>  
                 </span>
                 <button onClick={handleEditClick}>{btnText}</button>
